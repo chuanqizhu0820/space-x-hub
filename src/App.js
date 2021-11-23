b1098d84539cce548c062131839f5004a2fe0060
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-11-23 20:45:03 +0100

    Fix missing lines

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Provider } from "react-redux";
 import './App.css';
 import Rockets from "./components/Rockets";
+import { Missions } from './components/Missions';
+import store from './Redux/configureStore';
+
 
 function App() {
   return (
     <>
+      <Provider store={store}>
         <Rockets />
+        <Missions />
+      </Provider>
     </>
   );
 }
