67c364f25d6b71cc8984f3de2ba37befbb2c3a15
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-11-23 12:05:56 +0100

    Implement mission joining

--- src/Redux/missions/Missions.js
+++ src/Redux/missions/Missions.js
@@ -1,18 +1,37 @@
 const LOAD_MISSIONS = 'space-rockets-hub/missions/LOAD_MISSIONS';
+const RESERVED_MISSIONS = 'space-rockets-hub/missions/RESERVED_MISSIONS';
 const baseUrl = 'https://api.spacexdata.com/v3/missions';
 
 const initialState = {
   missions: [],
 };
 
+function reservedMission(state, action) {
+  const { missions } = state;
+  const index = missions.findIndex((mission) => mission.mission_id === action.id);
+  if (index !== -1) {
+    if (missions[index].reserved) {
+      missions[index].reserved = false;
+    } else missions[index].reserved = true;
+    return { ...state, missions };
+  }
+  return state;
+}
+
 export const missions = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_MISSIONS:
       return { ...state, missions: action.missions };
+    case RESERVED_MISSIONS:
+      return reservedMission(state, action);
     default:
       return state;
   }
 };
+
+export function reserved(id) {
+  return { type: RESERVED_MISSIONS, id };
+}
 
 export const loadMissions = (result) => {
   return { type: LOAD_MISSIONS, missions: result };
@@ -27,4 +46,4 @@ export const fetchApiData = () => async (dispatch) => {
   } catch (error) {
     dispatch(loadMissions({ missions: [] }));
   }
-}
+};
