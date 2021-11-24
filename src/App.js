38985048d818a1a4a520f77a42ccb4a5d1437b54
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-11-24 10:36:14 +0100

    Complete header styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import './App.css';
 import store from './Redux/configureStore';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 import { Missions } from "./components/Missions";
 import Rockets from "./components/Rockets";
 import { Profile } from "./components/Profile";
@@ -23,13 +23,15 @@ function App() {
         </div>
         <div className="col-6 d-flex flex-row justify-content-end align-items-center">
           <div>
-          <Link to="/rockets" className="links">Rockets</Link>
+          <NavLink to="/rockets" activeClassName="active" className="links">Rockets</NavLink>
           </div>
           <div>
-          <Link to="/missions" className="links">Missions</Link>
+          <NavLink to="/missions" activeClassName="active" className="links">Missions</NavLink>
           </div>
+          <span className="line"></span>
           <div>
-          <Link to="/profile" className="links">My Profile</Link>
+          <NavLink to="/profile" activeClassName="active"
+          className="links">My Profile</NavLink>
           </div>
         </div>
 
