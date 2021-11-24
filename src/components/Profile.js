d78e0e25d2301cf3d828ab3f481bf872e88e2b2f
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-11-24 10:01:21 +0000

    Add display of rocket reservation in the profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
+
 export function Profile(){
+    const rockInfo = useSelector(state=>state.rockets);
     return (
+        <>
         <h1>this is the profile page</h1>
+        <div>
+            {/* eslint-disable-next-line */}
+            {rockInfo.map(item => {
+                if (item.reserved){
+                    return <p>{item.rocket_name}</p>
+                }})}
+        </div>
+        </>
     )
 }
--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,23 +1,9 @@
-import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { reserveRocket, cancelRocket, loadRockets} from '../Redux/rockets/Rockets';
+import { reserveRocket, cancelRocket } from '../Redux/rockets/Rockets';
 
 export default function Rockets(){
     const dispatch = useDispatch();
-    useEffect( ()=> {
-        fetch("https://api.spacexdata.com/v3/rockets")
-        .then(response => response.json())
-        .then(data => {
-            for (const item of data){
-                item.reserved = false;
-            };
-            dispatch(loadRockets({info: data}))
-        }, 
-        error => console.log(error));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
-
     const rockInfo = useSelector(state => state.rockets);
     return (
         rockInfo.map(((item,idx, arr)=>(
